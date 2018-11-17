f0a8da02a34ad162a8989a8728bb19b3d55c6145
mahesh <mahesh@192.168.0.118>
2018-11-17 10:36:23 +0530

    Implemented Vertica Web Application per the requirement document

--- VerticaWebStaticContent/WebContent/resources/javascript/verticahome.js
+++ VerticaWebStaticContent/WebContent/resources/javascript/verticahome.js
@@ -3,6 +3,46 @@ function initiateClock() {
     var hours = today.getHours();
     var minutes = today.getMinutes();
     var seconds = today.getSeconds();
-    document.getElementById('clock').innerHTML = hours + ":" + minutes + ":" + seconds;
+    $("#clock").html( hours + ":" + minutes + ":" + seconds);
     var t = setTimeout(initiateClock, 500);
 }
+
+function centerFloatingPanel() {
+    $("#floatingPanel").css({
+        position: "absolute",
+        left: ($(window).width() - $("#floatingPanel").outerWidth()) / 2,
+        top: ($(window).height() - $("#floatingPanel").outerHeight()) / 2
+    });
+}
+
+$(function() {
+    console.log("Initializing the draggable property for the floating panel");
+    $("#floatingPanel").draggable({ containment: "parent"});
+    
+    $(".positionRadio").change(function () {
+        if(this.value == "centre") {
+            if($("#floatingPanel").hasClass("lowerRightPosition")) {
+                $("#floatingPanel").removeClass("lowerRightPosition");
+            }
+            centerFloatingPanel();
+        } else if (this.value == "lowerRight") {
+            if($("#floatingPanel").attr("style")) {
+                $("#floatingPanel").removeAttr("style");
+            }
+            $("#floatingPanel").addClass("lowerRightPosition");
+        }
+    });
+    
+    $(document).on('keypress', function(e){
+        if(e.keyCode === 27) {
+            console.log("Escape pressed. Hiding the floating panel");
+            $("#floatingPanel").hide();
+        } else if(e.keyCode === 13) {
+            console.log("Enter pressed. Showing the floating panel");
+            $("#floatingPanel").show();
+        }
+    });
+    
+    $(window).resize(centerFloatingPanel);
+    
+});
